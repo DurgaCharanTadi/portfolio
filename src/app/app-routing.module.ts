c2d9885bfd84c3a5512864589e045e40c63976de
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2024-06-20 18:37:24 -0500

    New Build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExperienceComponent } from './components/main-content/experience/experience.component';
 import { EducationComponent } from './components/main-content/education/education.component';
 import { CertificationsComponent } from './components/main-content/certifications/certifications.component';
+import { ProjectsComponent } from './components/main-content/projects/projects.component';
 
 const routes: Routes = [
   { path:'', component: ExperienceComponent },
   { path:'education', component: EducationComponent },
-  { path:'certifications', component: CertificationsComponent}
+  { path:'certifications', component: CertificationsComponent},
+  { path:'projects', component: ProjectsComponent}
 ];
 
 @NgModule({
--- src/app/components/main-content/projects/projects.component.ts
+++ src/app/components/main-content/projects/projects.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  open_youtubeClone(){
+    window.open('https://durgacharantadi.github.io/youtube-clone-ui/');
+  }
+  open_portfolioGit(){
+    window.open('https://github.com/DurgaCharanTadi/portfolio');
+  }
+  open_weatherApp(){
+    window.open('https://durgacharantadi.github.io/weather-app-reactjs/');
+  }
+  open_gitRepo(){
+    window.open('https://github.com/DurgaCharanTadi');
+  }
 }
