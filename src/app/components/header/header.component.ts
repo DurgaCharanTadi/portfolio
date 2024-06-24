476e8c0aa1093803340291dd2c0ecb9878dfe48e
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2024-06-24 09:55:36 -0500

    UI Updates

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   openMyResume(){
-    window.open('assets/files/DurgaCharanTadi_Resume.pdf','_blank');
+    window.open('assets/files/DurgaCharanTadi-Resume.pdf','_blank');
   }
 } 
